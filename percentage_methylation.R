3f523bcbc967728f48773ca4d8fe7de48fedc62c
Molly Brothers <mollybrothers@Mollys-iMac.local>
2020-11-06 12:02:40 -0800

    Update percentage_methylation

--- percentage_methylation.R
+++ percentage_methylation.R
@@ -6,14 +6,14 @@ colnames(dt) <- c("chrom", "start", "end", "name", "score",
                   "strand", "startCodon", "stopCodon", "color", 
                   "coverage", "percentage")
 
-#get a new data frame only containing chrom, start, end, coverage, and percentage
+#get a new data table only containing chrom, start, coverage, and percentage
 #filter out mitochondrial DNA (MT), rDNA (XII), and coverage < 15
 
-select_cols <- c("chrom", "start", "end", "coverage", "percentage")
+select_cols <- c("chrom", "start", "coverage", "percentage")
 relevant <- dt[chrom != "MT" & chrom != "XII" & coverage > 15, ..select_cols]
 
 #plot percentage of methylation in a particular region as a scatter plot
-#size of dot corresponds to amount of coverage
+#opacity of dot corresponds to amount of coverage
 control <- relevant[chrom == "X" & start > 50e3 & start < 73e3]
 ggplot(
   control,
@@ -38,4 +38,3 @@ ggplot(
   theme_minimal() +
   geom_point(position = "jitter", color = "mediumpurple4")
 
-#bin into 10bp bins, take average % methylation of those 10bp
