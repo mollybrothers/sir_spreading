996c0f9384ce7f3e9167fb10ea4f2174c7c1d01b
Molly Brothers <mollybrothers@Mollys-MacBook-Pro.local>
2020-11-08 14:06:35 -0800

    improved percentage_methylation.R and added descriptions to get_barcodes.py and percentage_methylation.R

--- RScripts/percentage_methylation.R
+++ RScripts/percentage_methylation.R
@@ -1,3 +1,16 @@
+#!/bin/R
+
+#######################
+# Author: Molly Brothers
+# Github: mollybrothers
+# Date: 2020-11-08
+#######################
+
+# With an input bedMethyl file:
+# (https://www.encodeproject.org/data-standards/wgbs/),
+# this script will plot the percentage of methylation
+# at each position as a scatterplot
+
 library(data.table)
 library(tidyverse)
 
@@ -14,27 +27,26 @@ relevant <- dt[chrom != "MT" & chrom != "XII" & coverage > 15, ..select_cols]
 
 #plot percentage of methylation in a particular region as a scatter plot
 #opacity of dot corresponds to amount of coverage
+plot_methylation <- function(data, chr) {
+  ggplot(
+    data,
+    aes(x = start, y = percentage, alpha = coverage)
+    ) +
+      theme_minimal() +
+      geom_point(position = "jitter", color = "mediumpurple4") +
+      xlab(sprintf("position on %s", chr)) +
+      ylab("% methylated reads")
+}
+
+#plot a negative control region
 control <- relevant[chrom == "X" & start > 50e3 & start < 73e3]
-ggplot(
-  control,
-  aes(x = start, y = percentage, alpha = coverage)
-  ) +
-  theme_minimal() +
-  geom_point(position = "jitter", color = "mediumpurple4")
+plot_methylation(control, "X")
 
+#plot HML
 HML <- relevant[chrom == "III" & start > 0 & start < 25e3]
-ggplot(
-  HML, 
-  aes(x = start, y = percentage, alpha = coverage)
-  ) +
-  theme_minimal() +
-  geom_point(position = "jitter", color = "mediumpurple4")
+plot_methylation(HML, "III")
 
+#plot HMR
 HMR <- relevant[chrom == "III" & start > 285e3 & start < 300e3]
-ggplot(
-  HMR,
-  aes(x = start, y = percentage, alpha = coverage)
-  ) +
-  theme_minimal() +
-  geom_point(position = "jitter", color = "mediumpurple4")
+plot_methylation(HMR, "III")
 
--- python_scripts/get_barcodes.py
+++ python_scripts/get_barcodes.py
@@ -1,5 +1,14 @@
 #!/bin/env python3
 
+###########################
+# Author: Molly Brothers
+# Github: mollybrothers
+# Date: 2020-11-08       
+###########################
+
+# This script uses the barcoding_summary.txt output from Oxford Nanopore's guppy_barcoder
+# to generate a list of readIDs that correspond to a provided barcode name (e.g. barcode01)
+
 import os
 import sys
 import argparse
@@ -15,7 +24,7 @@ in_file = open(input, 'r')
 out_dir = args.out_dir
 barcode = args.barcode
 
-#create a text file of readIDs that correspond to the barcode
+# create a text file of readIDs that correspond to the barcode
 with open(out_dir + barcode +'_readIDs' + '.txt', 'w') as out_file:
     for line in in_file:
         values = line.split()
