5b872083ce306ac0ef503b4044d02b7e7992ef51
Molly Brothers <mollybrothers@Mollys-iMac.local>
2020-12-01 12:05:25 -0800

    use coverage >10 instead of 15 and use a consistent alpha (instead of alpha = coverage)

--- RScripts/percentage_methylation.R
+++ RScripts/percentage_methylation.R
@@ -1,10 +1,10 @@
 #!/bin/R
 
-#######################
+#########################
 # Author: Molly Brothers
 # Github: mollybrothers
-# Date: 2020-11-12
-#######################
+# Date: 2020-12-01
+#########################
 
 # With an input bedMethyl file:
 # (https://www.encodeproject.org/data-standards/wgbs/),
@@ -14,28 +14,29 @@
 library(data.table)
 library(tidyverse)
 
-dt <- fread("/Volumes/brothers_seq/Nanopore/201012_Doudna/megalodon_outputv2/modified_bases.6mA.bed")
+dt <- fread("/Volumes/brothers_seq/Nanopore/201125_Turkey/megalodon_output_05/modified_bases.aggregate05.6mA.bed")
 colnames(dt) <- c("chrom", "start", "end", "name", "score", 
                   "strand", "startCodon", "stopCodon", "color", 
                   "coverage", "percentage")
 
 #get a new data table only containing chrom, start, coverage, and percentage
-#filter out mitochondrial DNA (MT), rDNA (XII), and coverage < 15
+#filter out mitochondrial DNA (MT) and coverage < 10
 
 select_cols <- c("chrom", "start", "coverage", "percentage")
-relevant <- dt[chrom != "MT" & chrom != "XII" & coverage > 15, ..select_cols]
+relevant <- dt[chrom != "MT" & coverage > 10, ..select_cols]
 
 #plot percentage of methylation in a particular region as a scatter plot
 #opacity of dot corresponds to amount of coverage
 plot_methylation <- function(data, chr) {
   ggplot(
     data,
-    aes(x = start, y = percentage, alpha = coverage)
+    aes(x = start, y = percentage)
     ) +
-    geom_point(position = "jitter", color = "mediumpurple4") +
+    geom_point(position = "jitter", color = "mediumpurple4", alpha = 1/4) +
     theme_minimal() +
-    labs(x = sprintf("position on %s", chr),
-           y = "% methylated reads")
+    labs(title = "unfused EcoGII (JRY12838)",
+         x = sprintf("position on chr %s", chr),
+         y = "% methylated reads")
 }
 
 #plot a negative control region
