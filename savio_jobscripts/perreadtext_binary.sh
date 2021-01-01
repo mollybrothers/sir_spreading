899fa1b320745bf0a7da5176fe705e8175ea12da
koenvandenberge <koen.vandenberge@ugent.be>
2021-01-01 13:02:10 +0100

    add draft script to create binary perread files

---
+++ savio_jobscripts/perreadtext_binary.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#SBATCH --job-name=perReadBinary
+#SBATCH --account=fc_nanosir
+#SBATCH --partition=savio2_htc
+#SBATCH --output=perReadBinary.out
+#SBATCH --error=perREadBinary.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=koenvdberge@berkeley.edu
+#SBATCH --time=44:00:00
+
+cd /global/scratch/molly_brothers/201125_Turkey/megalodon_output_06/
+awk '(NR>1) && ($5 > -0.2231436) || ($5 < -1.609438) ' per_read_modified_base_calls.txt > per_read_modified_base_calls_binary.txt
+
+cd /global/scratch/molly_brothers/201125_Turkey/megalodon_output_07/
+awk '(NR>1) && ($5 > -0.2231436) || ($5 < -1.609438) ' per_read_modified_base_calls.txt > per_read_modified_base_calls_binary.txt
+
+
+#  perreadtext_binary.sh
+#
+#
+#  Created by Koen Van den Berge on 1/1/21.
+#
