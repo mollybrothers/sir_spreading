777e954f79812df96c399fc517c316ad407a6d06
Molly Brothers <mollybrothers@Mollys-iMac.local>
2021-01-27 10:32:34 -0800

    make plotting bins/bars into a function

--- RScripts/percentage_methylation.R
+++ RScripts/percentage_methylation.R
@@ -3,7 +3,7 @@
 #########################
 # Author: Molly Brothers
 # Github: mollybrothers
-# Date: 2021-01-26
+# Date: 2021-01-27
 #########################
 
 # With an input bedMethyl file:
@@ -28,64 +28,75 @@ relevant <- dt[chrom != "MT" & coverage > 10, ..select_cols]
 
 #plot percentage of methylation in a particular region as a scatter plot
 #opacity of dot corresponds to amount of coverage
-plot_methylation <- function(data, chr) {
-  ggplot(
-    data,
-    aes(x = start, y = percentage)
-    ) +
-    geom_point(position = "jitter", color = "mediumpurple4", alpha = 1/4) +
-    ylim(0,100) +
-    theme_minimal() +
-    theme(panel.grid.minor = element_blank(),
-          text = element_text(size=15)) +
-    labs(title = "SIR3-EcoGII (JRY13027)",
+plot_title = ""
+numbreaks = 150
+
+plot_methylation_dot <- function(data, chr) {
+  ggplot(data, aes(x = start, y = percentage)) +
+    geom_point(color = "mediumpurple4", alpha = 1/4) +
+    scale_x_continuous(limits = c(min(data$start), max(data$start)), expand = c(0, 0)) +
+    scale_y_continuous(limits = c(0,100), expand = c(0, 0)) +
+    theme(panel.background = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank(),
+          axis.line = element_line(color = "black"),
+          text = element_text(size=15, color = "black")) +
+    labs(title = plot_title,
+         x = sprintf("position on chr %s", chr),
+         y = "% methylated reads")
+}
+
+plot_methylation_bars <- function(data, chr) {
+  #break data into bins, find the mean and sd of % methylated reads in each bin
+  data$window <- cut(data$start, breaks = numbreaks)
+  data_mean <- aggregate(data$percentage, by = list(data$window), function(x){ mean(x)})
+  data_sd<- aggregate(data$percentage, by = list(data$window), function(x){ sd(x)})
+  
+  #make a data.frame of the stats found above and clean up for plotting
+  data_stats <- data.frame(interval = data_mean[,1], mean = data_mean[,2], upper = data_mean[,2]+data_sd[, 2], lower = data_mean[,2]-data_sd[, 2])
+  data_stats$interval <- gsub("[(]", "", data_stats$interval)
+  data_stats$interval <- gsub("[,].*", "", data_stats$interval)
+  data_stats$interval <- as.numeric(data_stats$interval)
+  
+  #get the points that have 100% reads methylated
+  data_full <- data[data$percentage == 100, ]
+  
+  #plot the bins and the points that are at 100%
+  ggplot(data_stats, aes(x = interval, y = mean)) +
+    geom_point(color = "mediumpurple4") +
+    geom_point(data = data_full, aes(x = start, y = percentage), color = "mediumpurple4", inherit.aes = FALSE) +
+    geom_errorbar(aes(ymin = lower, ymax = upper), color = "mediumpurple4", inherit.aes = TRUE) + 
+    theme(panel.background = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank(),
+          axis.line = element_line(color = "black"),
+          text = element_text(size=15, color = "black")) +
+    labs(title = plot_title,
          x = sprintf("position on chr %s", chr),
          y = "% methylated reads")
 }
 
 #plot a negative control region
 control <- relevant[chrom == "X" & start > 50e3 & start < 75e3]
-plot_methylation(control, "X")
+plot_methylation_dot(control, "X")
+plot_methylation_bars(control, "X")
 
 #plot HML
 HML <- relevant[chrom == "III" & start > 0 & start < 25e3]
-plot_methylation(HML, "III")
-dim(HML)
-
-HML$window <- cut(HML$start, breaks = 150)
-HMLmean <- aggregate(HML$percentage, by = list(HML$window), function(x){ mean(x[x != 0])})
-HMLsd<- aggregate(HML$percentage, by = list(HML$window), function(x){ sd(x[x != 0])})
-
-HMLfinal <- data.frame(interval = HMLmean[,1], mean = HMLmean[,2], upper = HMLmean[,2]+HMLsd[, 2], lower = HMLmean[,2]-HMLsd[, 2])
-HMLfinal$interval <- gsub("[(]", "", HMLfinal$interval)
-HMLfinal$interval <- gsub("]", "", HMLfinal$interval)
-HMLfinal$start <- as.numeric(unlist(lapply(str_split(HMLfinal$interval, ","), "[", 1)))
-HMLfull <- HML[HML$percentage == 100, ]
-
-HML$window <- gsub("[(]", "", HML$window)
-HML$window <- gsub("]", "", HML$window)
-HML$window <- as.numeric(unlist(lapply(str_split(HML$window, ","), "[", 1)))
-
-
-ggplot(HMLfinal, aes(x = start, y = mean)) +
-  geom_point(color = "mediumpurple4") +
-  geom_point(data = HMLfull, aes(x = start, y = percentage), color = "mediumpurple4", inherit.aes = FALSE) +
-  geom_errorbar(aes(ymin = lower, ymax = upper), color = "mediumpurple4", inherit.aes = TRUE) + 
-  scale_x_continuous(breaks = seq(0, 25000, 2000)) +
-  theme_bw() + 
-  theme(axis.text.x = element_text(angle = 45, size = 10, vjust = 1, hjust = 1)) +
-  labs(title = "SIR3-EcoGII (JRY13027)",
-       x = "position on chr III",
-       y = "% methylated reads")
+plot_methylation_dot(HML, "III")
+plot_methylation_bars(HML, "III")
 
 #plot HMR
 HMR <- relevant[chrom == "III" & start > 285e3 & start < 300e3]
-plot_methylation(HMR, "III")
+plot_methylation_dot(HMR, "III")
+plot_methylation_bars(HMR, "III")
 
 #plot telomere
 tel6R <- relevant[chrom == "VI" & start > 250e3 & start < 270e3]
-plot_methylation(tel6R, "VI")
+plot_methylation_dot(tel6R, "VI")
+plot_methylation_bars(tel6R, "VI")
 
 #plot chromosome
 chrXV <- relevant[chrom == "XV"]
-plot_methylation(chrXV, "XV")
+plot_methylation_dot(chrXV, "XV")
+plot_methylation_bars(chrXV, "XV")
