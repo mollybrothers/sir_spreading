ef025d20564da109f6693c2363d1ebd5a1379229
Molly Brothers <mollybrothers@Mollys-iMac.local>
2021-01-27 11:35:26 -0800

    make control region internal to chrIII

--- RScripts/percentage_methylation.R
+++ RScripts/percentage_methylation.R
@@ -28,8 +28,7 @@ relevant <- dt[chrom != "MT" & coverage > 10, ..select_cols]
 
 #plot percentage of methylation in a particular region as a scatter plot
 #opacity of dot corresponds to amount of coverage
-plot_title = ""
-numbreaks = 150
+plot_title = "SIR3-EcoGII (JRY13027)"
 
 plot_methylation_dot <- function(data, chr) {
   ggplot(data, aes(x = start, y = percentage)) +
@@ -40,17 +39,18 @@ plot_methylation_dot <- function(data, chr) {
           panel.grid.minor = element_blank(),
           panel.grid.major = element_blank(),
           axis.line = element_line(color = "black"),
-          text = element_text(size=15, color = "black")) +
+          text = element_text(size=15, color = "black", family = "Arial")) +
     labs(title = plot_title,
          x = sprintf("position on chr %s", chr),
          y = "% methylated reads")
 }
 
 plot_methylation_bars <- function(data, chr) {
-  #break data into bins, find the mean and sd of % methylated reads in each bin
+  #break data into 200bp bins, find the mean and sd of % methylated reads in each bin
+  numbreaks <- as.integer((max(data$start) - min(data$start)) / 200)
   data$window <- cut(data$start, breaks = numbreaks)
   data_mean <- aggregate(data$percentage, by = list(data$window), function(x){ mean(x)})
-  data_sd<- aggregate(data$percentage, by = list(data$window), function(x){ sd(x)})
+  data_sd <- aggregate(data$percentage, by = list(data$window), function(x){ sd(x)})
   
   #make a data.frame of the stats found above and clean up for plotting
   data_stats <- data.frame(interval = data_mean[,1], mean = data_mean[,2], upper = data_mean[,2]+data_sd[, 2], lower = data_mean[,2]-data_sd[, 2])
@@ -70,16 +70,16 @@ plot_methylation_bars <- function(data, chr) {
           panel.grid.minor = element_blank(),
           panel.grid.major = element_blank(),
           axis.line = element_line(color = "black"),
-          text = element_text(size=15, color = "black")) +
+          text = element_text(size = 15, color = "black", family = "Arial")) +
     labs(title = plot_title,
          x = sprintf("position on chr %s", chr),
          y = "% methylated reads")
 }
 
 #plot a negative control region
-control <- relevant[chrom == "X" & start > 50e3 & start < 75e3]
-plot_methylation_dot(control, "X")
-plot_methylation_bars(control, "X")
+control <- relevant[chrom == "III" & start > 80e3 & start < 105e3]
+plot_methylation_dot(control, "III")
+plot_methylation_bars(control, "III")
 
 #plot HML
 HML <- relevant[chrom == "III" & start > 0 & start < 25e3]
@@ -87,7 +87,7 @@ plot_methylation_dot(HML, "III")
 plot_methylation_bars(HML, "III")
 
 #plot HMR
-HMR <- relevant[chrom == "III" & start > 285e3 & start < 300e3]
+HMR <- relevant[chrom == "III" & start > 280e3 & start < 305e3]
 plot_methylation_dot(HMR, "III")
 plot_methylation_bars(HMR, "III")
 
