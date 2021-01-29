00ee24eb69e84ec591c2364c323fe728769e5b79
Molly Brothers <mollybrothers@Mollys-iMac.local>
2021-01-28 16:59:56 -0800

    clean up code and make plotting into function

--- RScripts/nucleosome_occupancy.R
+++ RScripts/nucleosome_occupancy.R
@@ -3,7 +3,7 @@
 #########################
 # Author: Molly Brothers
 # Github: mollybrothers
-# Date: 2020-12-13
+# Date: 2021-01-28
 #########################
 
 # IN PROGRESS
@@ -13,56 +13,89 @@
 library(data.table)
 library(tidyverse)
 
-#read in percentage methylation data
+####################
+####READ IN DATA####
+####################
+
+#percentage methylation data
 # Molly's path
-# meth <- fread("/Volumes/brothers_seq/Nanopore/201012_Doudna/201012_Doudna_megalodon_output_00/modified_bases.6mA.bed")
+meth <- fread("/Volumes/brothers_seq/Nanopore/201012_Doudna/megalodon_output_00/modified_bases.6mA.bed")
 # Savio path
 # meth <- fread("/global/scratch/molly_brothers/201012_Doudna/megalodon_output_00/modified_bases.6mA.bed")
 # Koen's path
-meth <- fread("~/data/molly/modified_bases.6mA.bed")
+# meth <- fread("~/data/molly/modified_bases.6mA.bed")
 colnames(meth) <- c("chrom", "start", "end", "name", "score", 
                     "strand", "startCodon", "stopCodon", "color", 
                     "coverage", "percentage")
-
 meth_cols <- c("chrom", "start", "coverage", "percentage")
 filtered_meth <- meth[coverage > 10, ..meth_cols]
 
-#read in nucleosome occupancy data from Chereji et al 2018
+#nucleosome occupancy data from Chereji et al 2018
 # Molly's path
-# nucs <- fread("/Volumes/brothers_seq/not_my_data/GSE97290_Henikoff_Chereji_2018/Chereji_Occupancy_rep1_singlebp.bedGraph")
+nucs <- fread("/Volumes/brothers_seq/not_my_data/GSE97290_Henikoff_Chereji_2018/Chereji_Occupancy_rep1_singlebp.bedGraph")
 # Savio path
 # nucs <- fread("/global/scratch/molly_brothers/not_my_data/GSE97290_Henikoff_Chereji_2018/Chereji_Occupancy_rep1_singlebp.bedGraph")
 # Koen's path
-nucs <- fread("~/data/molly/Chereji_Occupancy_rep1_singlebp.bedGraph")
+# nucs <- fread("~/data/molly/Chereji_Occupancy_rep1_singlebp.bedGraph")
 colnames(nucs) <- c("chrom", "start", "end", "occupancy")
 
-#plot percentage methylation as a column chart
-HMR <- filtered_meth[chrom == "III" & start > 291e3 & start < 294e3]
+#################################################
+####PLOT METHYLATION VS. NUCLEOSOME OCCUPANCY####
+#################################################
+
+methyl_vs_nucs <- function (methyl_data, nuc_data) {
+  plot(x=methyl_data$start, y=rep(-3,3,length.out=nrow(methyl_data)), 
+       type="n", ylim=c(-3,4), 
+       ylab="Scaled average methylation / occupancy",
+       xlab="Chromosome position")
+  legend("topleft", c("Methylation", "Nucleosome occupancy"), 
+         col=c("mediumpurple4", "grey50"), lty=1, cex=1,
+         bty='n', lwd=3)
+  
+  # Methylation
+  loMethyl <- loess(percentage/100 ~ start, 
+                    data = methyl_data, 
+                    weights = methyl_data$coverage, 
+                    enp.target = 50)
+  lines(x=loMethyl$x[order(loMethyl$x)], y=scale(loMethyl$fitted[order(loMethyl$x)]), 
+        col="mediumpurple4", lwd=3)
+  
+  # Nucleosome occupancy
+  loOccup <- loess(occupancy/max(occupancy) ~ start, 
+                   data=nuc_data, 
+                   enp.target = 50)
+  lines(x=loOccup$x, y=scale(loOccup$fitted), col="grey70", lwd=3)
+}
+
+#control
+#why does the control region show methylation?
+ctrl_meth <- filtered_meth[chrom == "III" & start > 96e3 & start < 99e3]
+ctrl_nucs <- nucs[chrom == "chrIII" & start > 96e3 & start < 99e3]
+methyl_vs_nucs(ctrl_meth, ctrl_nucs)
+
+#HMR
+HMR_meth <- filtered_meth[chrom == "III" & start > 291e3 & start < 294e3]
 HMR_nucs <- nucs[chrom == "chrIII" & start > 291e3 & start < 294e3]
+methyl_vs_nucs(HMR_meth, HMR_nucs)
 
-ggplot() +
-  geom_col(HMR, mapping = aes(x = start, y = percentage), color = "mediumpurple4") +
-  geom_col(HMR_nucs, mapping = aes(x = start, y = occupancy * 20), color = "black") +
-  theme_minimal() +
-  labs(title = "SIR3-EcoGII (JRY13027)",
-       x = "position on chr III",
-       y = "% methylated reads")
+#HML
+HML_meth <- filtered_meth[chrom == "III" & start > 11e3 & start < 14e3]
+HML_nucs <- nucs[chrom == "chrIII" & start > 11e3 & start < 14e3]
+methyl_vs_nucs(HML_meth, HML_nucs)
 
+#tel6R
+tel6R_meth <- filtered_meth[chrom == "VI" & start > 265e3 & start < 268e3]
+tel6R_nucs <- nucs[chrom == "chrVI" & start > 265e3 & start < 268e3]
+methyl_vs_nucs(tel6R_meth, tel6R_nucs)
 
-plot(x=HMR$start, y=rep(-3,3,length.out=nrow(HMR)), 
-     type="n", ylim=c(-3,3), 
-     ylab="Scaled average methylation / occupancy",
-     xlab="Chromosome position")
-# Methylation
-loMethyl <- loess(percentage/100 ~ start, data=HMR, weights=HMR$coverage, enp.target = 50)
-lines(x=loMethyl$x[order(loMethyl$x)], y=scale(loMethyl$fitted[order(loMethyl$x)]), 
-      col="orange", lwd=3/2)
-# Nucleosome occupancy
-loOccup <- loess(occupancy/max(occupancy) ~ start, data=HMR_nucs, enp.target = 50)
-lines(x=loOccup$x, y=scale(loOccup$fitted), col="darkseagreen3", lwd=3/2)
-legend("topleft", c("Methylation", "Nucleosome occupancy"), 
-       col=c("orange", "darkseagreen3"), lty=1, cex=1,
-       bty='n', lwd=2)
+#tel8L
+tel10L_meth <- filtered_meth[chrom == "X" & start > 0 & start < 5e3]
+tel10L_nucs <- nucs[chrom == "chrX" & start > 0 & start < 5e3]
+methyl_vs_nucs(tel10L_meth, tel10L_nucs)
+
+#####################################
+####SCATTER PLOT OF FITTED VALUES####
+#####################################
 
 # scatter plot of fitted values gives us a confused snake.
 dfGrid <- data.frame(start = seq(291000, 294000), length.out=1000)
