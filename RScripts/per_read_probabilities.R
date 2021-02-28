dd4a38b90167c1a73799c11291edf991a9b744d2
Molly Brothers <mollybrothers@Stanley-392-W8905HCB0TG.local>
2021-02-28 13:40:58 -0800

    remove plotting raw probs, remove read filtering, add prelim. hclust

--- RScripts/per_read_probabilities.R
+++ RScripts/per_read_probabilities.R
@@ -3,7 +3,7 @@
 #########################
 # Author: Molly Brothers
 # Github: mollybrothers
-# Date: 2021-02-14 <3
+# Date: 2021-02-28
 #########################
 
 #######################################################################################
@@ -18,31 +18,21 @@ library(tidyverse)
 library(wesanderson)
 
 mega_directory <- "/Volumes/brothers_seq/Nanopore/201125_Turkey/megalodon_output_07/"
-chr <- "VI" #which chromosome?
+chr <- "III" #which chromosome?
 
-# find all the reads with a qscore < 9 and filter out those reads from the input chr.txt file
-summary <- fread(sprintf("%ssequencing_summary.txt", mega_directory),
-                 select = c('read_id', 'mean_qscore_template'))
 
-bad_reads <- summary[mean_qscore_template <= 9, read_id]
+probs <- fread(sprintf("%schr%s.txt", mega_directory, chr),
+                    select = c(1, 2, 3, 4, 5),
+                    col.names = c("read_id", "chrm", "strand", "pos", "mod_log_prob"))
 
-unfiltered <- fread(sprintf("%schr%s.txt", mega_directory, chr),
-                    select = c(1, 2, 4, 5),
-                    col.names = c("read_id", "chrm", "pos", "mod_log_prob"))
-
-filtered <- unfiltered[!(read_id %in% bad_reads)]
-
-#1. convert log probabilities into probabilities
-#2. add a binary column; if mod_prob is >80, set as methylated (TRUE). if < 80, set as unmethylated (FALSE)
-#3. add start and end positions for each read_id
-#4. find the average methylation of each read
-#5. order by average number of A's methylated then read_id
-probs <- filtered[, list(read_id, pos, mod_prob = 10 ^ mod_log_prob)][
-  , binary := ifelse(mod_prob > 0.8, "m6A", "A")][
+#1. create a binary column; if 10^mod_log_prob is >0.8, set as methylated (m6A). if < 0.8, set as unmethylated (A)
+#2. add start and end positions for each read_id
+#3. order by position
+probs <- probs[, methylation := ifelse(10^mod_log_prob > 0.8, TRUE, FALSE)][
+  , list(read_id, pos, methylation, strand)][
     , start_pos := min(pos), by = read_id][
       , end_pos := max(pos), by = read_id][
-        , avg_methyl := mean(binary == "m6A"), by = read_id][
-          order(-avg_methyl, read_id)]
+        order(pos)]
 
 #extract each unique read_id to set the order (in same order as in the data table to start with) for single read plots
 read_names <- unique(probs$read_id)
@@ -85,21 +75,7 @@ plot_prob(HMR_avg, "HMR")
 ####PLOT SINGLE READS####
 #########################
 
-#plot functions
-probs_pal <- wes_palette("Zissou1", 2, type = "continuous")
-binary_pal <- wes_palettes$Moonrise1
-
-plot_probs <- function(data) {
-  ggplot(data, aes(x = pos, y = read_id, color = mod_prob)) +
-    geom_point(shape = 15) +
-    theme(axis.text.y = element_blank(),
-          axis.ticks.y = element_blank(),
-          axis.title.y = element_blank(),
-          axis.title.x = element_blank(),
-          panel.grid = element_blank()) +
-    scale_color_gradientn(colors = probs_pal, name = "m6A probability")
-}
-
+#plot function
 plot_binary <- function(data) {
   ggplot(data, aes(x = pos, y = read_id, color = binary)) +
     geom_point(shape = 15) +
@@ -121,7 +97,6 @@ control_region <- c(95e3, 100e3)
 control <- probs[start_pos <= control_region[1]][end_pos >= control_region[2]][
   pos %between% control_region]
 plot_binary(control)
-plot_probs(control)
 
 #plot HML
 HML_region <- c(10.5e3, 15.5e3)
@@ -139,7 +114,6 @@ hmlp <- plot_binary(HML)
 hmlp
 hmlp + geom_vline(xintercept = HML_silencers) #+
 #geom_vline(xintercept = HML_linkers, size = 0.3, color = "black")
-plot_probs(HML)
 
 #plot HMR
 HMR_region <- c(291e3, 296e3)
@@ -148,12 +122,13 @@ HMR_linkers = c(291100, 291312, 291644, 291863, 292129, 292322,
                 292498, 292921, 293078, 293227, 293440, 293633, 293841, 294155,
                 294515, 294699, 295239, 295555, 295743, 295906)
 
-HMR <- probs[start_pos <= HMR_region[1]][end_pos >= HMR_region[2]][
+HMR_plus <- probs[strand == "+"][start_pos <= HMR_region[1]][end_pos >= HMR_region[2]][
   pos %between% HMR_region]
-hmrp <- plot_binary(HMR)
+HMR_minus <- probs[strand == "-"][start_pos <= HMR_region[1]][end_pos >= HMR_region[2]][
+  pos %between% HMR_region]
+hmrp <- plot_binary(HMR_plus)
 hmrp
 hmrp + geom_vline(xintercept = HMR_silencers)
-plot_probs(HMR)
 
 #plot tel3L
 tel3L_region <- c(300, 5e3)
@@ -165,3 +140,38 @@ plot_binary(tel3L)
 tel6R_region <- c(265e3, 270e3)
 tel6R <- probs[start_pos <= tel6R_region[1]][end_pos >= tel6R_region[2]][pos %between% tel6R_region]
 plot_binary(tel6R)
+
+###############################
+####HIERARCHICAL CLUSTERING####
+###############################
+
+#convert read_ids and positions to factors
+HMR_plus$read_id <- factor(HMR_plus$read_id)
+HMR_plus$pos <- factor(HMR_plus$pos)
+HMRplus_readnames <- unique(HMR_plus$read_id)
+HMRplus_positions <- unique(HMR_plus$pos)
+
+#create a new dataframe with positions as column names and read_ids as row names
+new_HMRplus <- data.frame()
+for(i in HMRplus_positions){
+  new_HMRplus[,i] <- NA
+}
+for(i in HMRplus_readnames){
+  new_HMRplus[i,] <- logical()
+}
+
+#move the methylation data from the original dataframe to the new dataframe
+for(x in HMRplus_readnames){
+  for(y in HMRplus_positions){
+    if(any(HMR_plus$read_id == x & HMR_plus$pos == y) == FALSE){
+      next
+    }
+    new_HMRplus[as.character(x),as.character(y)] <- HMR_plus[which(HMR_plus$read_id == x & HMR_plus$pos == y),]$methylation
+  }
+}
+
+#calculate distances and plot the resulting dendrogram
+HMRplus_distances <- dist(new_HMRplus, method = "binary")
+plot(hclust(HMRplus_distances, method = "average"))
+
+# TO DO: figure out how to apply the clustering/distances to levels to plot the single reads by similarity/difference
