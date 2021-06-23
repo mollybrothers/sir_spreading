a3d641f222b5e7ee6ab17c004e1488f596c1be68
mollybrothers <molly_brothers@berkeley.edu>
2021-06-23 10:29:15 -0700

    parallelize per-read-text script

--- local_jobscripts/run-mega_perreadtext.sh
+++ local_jobscripts/run-mega_perreadtext.sh
@@ -2,6 +2,6 @@
 
 source activate megalodon-env
 
-MEGALODON_DIRECTORY="/home/mbrothers/nanopore/210607_Dotty/megalodon_output_01"
+EXP="/home/mbrothers/nanopore/210607_Dotty"
 
-megalodon_extras per_read_text modified_bases $MEGALODON_DIRECTORY
+ls -d "$exp/megalodon_output_"* | parallel -u -I{} megalodon_extras per_read_text modified_bases {}
